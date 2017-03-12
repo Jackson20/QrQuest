ab32d6e4c179d98920c7f4bfad99f5a84b111308
Michael Hnap <mike.gnap.r@gmail.com>
2017-03-12 19:27:34 +0200

    Added logic for changing model's visibility.

---
+++ Scripts/GameWorld/ModelGameplay.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class ModelGameplay : MonoBehaviour
+{
+    private Model m_model { get; set; }
+    private Vector3 m_defaultLocalScale;
+
+    private void Start()
+    {
+        m_defaultLocalScale = transform.localScale;
+        transform.localScale = new Vector3(0, 0, 0);
+    }
+
+    public void LoadModel(Model item)
+    {
+        m_model = item;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "PlayerRadius")
+        {
+            transform.localScale = m_defaultLocalScale;
+        }
+    }
+}
